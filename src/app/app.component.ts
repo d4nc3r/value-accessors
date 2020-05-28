2541ce7940b4b943cedc1388159a8406a4d76605
Jamie Mitch <7iny.d4nc3r@gmail.com>
2020-05-28 08:20:46 -0400

    removed code from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,9 @@ import { tap } from 'rxjs/operators';
 })
 export class AppComponent {
   stepperForm: FormGroup;
-  selectedPets: FormControl;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
-  // fake code table :)
-  colors = [ 'red', 'blue', 'green', 'purple', 'orange' ];
+
   // this is the data coming in from an API, it should not be manipulated!
   pets = [
     { id: 1, name: 'Mitzi', species: 'cat', age: '1', gender: 'F', altered: true },
@@ -22,14 +20,9 @@ export class AppComponent {
   ];
 
   ngOnInit() {
-    this.selectedPets = new FormControl([1, 3], validateSelection);
     this.firstFormGroup = new FormGroup({
-      // validator will be custom, we need a non-empty array at the end
-      selectedPets: this.selectedPets
     });
     this.secondFormGroup = new FormGroup({
-      // validator will be custom, need a way to indicate something is wrong w/ the child form
-      petData: new FormControl()
     });
     this.stepperForm = new FormGroup({
       firstStep: this.firstFormGroup,
@@ -37,10 +30,4 @@ export class AppComponent {
     });
   }
 
-}
-
-function validateSelection(control: FormControl) {
-  // test for empty array
-  const isNotEmpty = control.value.length > 0;
-  return isNotEmpty ? null : { validateSelection: { valid: false } };
 }
