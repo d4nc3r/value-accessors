5cf10e1808e6af915e7598fbe0d72816e5c3513a
Jamie Mitch <7iny.d4nc3r@gmail.com>
2020-05-28 08:23:51 -0400

    removed pet-selector and pet-data code

--- src/app/pet-data/pet-data.component.ts
+++ src/app/pet-data/pet-data.component.ts
@@ -1,77 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { NG_VALUE_ACCESSOR, ControlValueAccessor, FormGroup, FormControl, Validators } from '@angular/forms';
-import { tap } from 'rxjs/operators';
-
-import { Pet } from '../models';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pet-data',
-  providers: [{
-    provide: NG_VALUE_ACCESSOR,
-    useExisting: PetDataComponent,
-    multi: true
-  }],
   templateUrl: './pet-data.component.html',
   styleUrls: ['./pet-data.component.scss']
 })
-export class PetDataComponent implements OnInit, ControlValueAccessor {
-  selectedPets: number[];
-  petInfoForm = new FormGroup({});
-  @Input() pets: Pet[];
-  @Input() set selectedPetIds(value: number[]) {
-    // each time the selectedPets array changes, update the form
-    this.selectedPets = value;
-    
-    // loop thru the pets
-    this.pets.forEach(pet => {
-      const isSelected = this.selectedPets.includes(pet.id);
-      // if pet is selected, ensure a FormGroup exists
-      isSelected ? this.addPetFormGroup(pet)
-      // if pet is not selected, remove FormGroup (if it already exists)
-        : this.removePetFormGroup(pet.id);
-    })
-  }
-  onChange: (value: any) => void;
+export class PetDataComponent implements OnInit {
 
   ngOnInit() {
-    this.petInfoForm.valueChanges.pipe(
-      tap(value => {
-        if(this.onChange) {
-          this.onChange(value);
-        }
-      })
-    ).subscribe();
-  }
-
-  registerOnTouched(fn) { }
-  registerOnChange(fn) { this.onChange = fn }
-  writeValue(value) { 
-    console.log('write value for pet-data');
-  }
-
-  addPetFormGroup(pet: Pet) {
-    const petId = pet.id.toString();
-    // if form group doesn't exist, add it
-    if(!this.petInfoForm.get(petId)) {
-      this.petInfoForm.addControl(petId, new FormGroup({
-        age: new FormControl(pet.age),
-        gender: new FormControl(pet.gender, Validators.required),
-        altered: new FormControl(pet.altered)
-      }));
-    }
-  }
-
-  removePetFormGroup(petId: number) {
-    // if form group exists, remove it
-
-  }
-
-  getPet(petId: number) {
-    return this.pets.find(pet => pet.id === petId);
-  }
-
-  getFormGroup(petId: number) {
-    return this.petInfoForm.get(petId.toString());
   }
 
 }
--- src/app/pet-selector/pet-selector.component.ts
+++ src/app/pet-selector/pet-selector.component.ts
@@ -1,75 +1,13 @@
-import { Component, Input, HostBinding, OnInit } from '@angular/core';
-import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
-import { tap } from 'rxjs/operators';
-
-import { Pet } from '../models';
-import { MatTableDataSource } from '@angular/material/table';
-import { SelectionModel } from '@angular/cdk/collections';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pet-selector',
-  providers: [{
-    provide: NG_VALUE_ACCESSOR,
-    useExisting: PetSelectorComponent,
-    multi: true
-  }],
   templateUrl: './pet-selector.component.html',
   styleUrls: ['./pet-selector.component.scss']
 })
-export class PetSelectorComponent implements OnInit, ControlValueAccessor {
-  @HostBinding() class = "db mb-4";
-  @Input() pets: Pet[];
-  dataSource: MatTableDataSource<Pet>;
-  displayedColumns = ['select', 'name', 'species'];
-  selection = new SelectionModel<Pet>(true, []);
-  onChange: (value: any) => void;
+export class PetSelectorComponent implements OnInit {
 
   ngOnInit() {
-    this.dataSource = new MatTableDataSource<Pet>(this.pets);
-    this.selection.changed.pipe(
-      tap(() => this.sendSelectedIds())
-    ).subscribe();
-  }
-
-  registerOnTouched(fn) { }
-  registerOnChange(fn) { this.onChange = fn }
-
-  /*
-   * this will get called anytime the FormControl value changes outside of this component,
-   * for example, if there is an initial value when the controls is created, the control gets reset, 
-   * or if setValue/patchValue gets called  
-   */
-  writeValue(selectedIds: number[]) {
-    this.dataSource.data.forEach(row => {
-      // if the row's id is in the selectedIds, select it, otherwise deselect it
-      selectedIds.includes(row.id) ? this.selection.select(row) : this.selection.deselect(row);
-    });
-
-  }
-
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
-  }
-
-  masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSource.data.forEach(row => this.selection.select(row));
-  }
-
-  checkboxLabel(row?: Pet): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
-    }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} ${row.name}`;
-  }
-
-  sendSelectedIds() {
-    if (this.onChange) {
-      this.onChange(this.selection.selected.map(pet => pet.id));
-    }
   }
 
 }
